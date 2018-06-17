25d17021c6787f3db6fc4b28d5bfcca594004c11
Torlopov Andrey <Torlopov.andrey@gmail.com>
2018-06-17 22:30:35 +0300

    fix score label

--- src/EldritchHorrorCards/EldritchHorrorCards/MonsterDetailViewController.swift
+++ src/EldritchHorrorCards/EldritchHorrorCards/MonsterDetailViewController.swift
@@ -3,14 +3,12 @@ class MonsterDetailViewController: UIViewController {
     
     @IBOutlet private var bgImageView: UIImageView!
     @IBOutlet private var nameLabel: UILabel!
-    @IBOutlet private var scoreLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         guard let _item = monster else { fatalError("No user to display.") }
         bgImageView.loadImageAtURL(monster.imageURLString, withDefaultImage: monster.placeholderImage)
-        nameLabel.text = _item.name
-        scoreLabel.text = "\(_item.score)"
+        nameLabel.text = "\(_item.name) (\(_item.score))"
     }
     
     @IBAction func callMonsterAction(_ sender: AnyObject) {
