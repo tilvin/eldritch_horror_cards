1dedf51f40a0d3aad88ec30c6bc181bfd917e880
cinichka85 <cinichka85@gmail.com>
2018-07-26 19:04:10 +0300

    fix AuthProvider

--- EldritchHorrorCards/EldritchHorrorCards/Common/Public/Providers/AuthProvider.swift
+++ EldritchHorrorCards/EldritchHorrorCards/Common/Public/Providers/AuthProvider.swift
@@ -9,62 +9,71 @@
 import Foundation
 
 protocol AuthProviderProtocol {
-	var token: String { get set }
-	var login: String { get set }
-	
-	func loadToken() -> Bool
-	func authorize(with login: String, password: String, completion: @escaping (Bool) -> Void)
-	func logout(error: Swift.Error?)
-	func clear()
+    var token: String { get set }
+    var login: String { get set }
+    
+    func loadToken() -> Bool
+    func authorize(with login: String, password: String, completion: @escaping (Bool) -> Void)
+    func logout(error: Swift.Error?)
+    func clear()
 }
 
 extension AuthProviderProtocol {
-	var isAuthorized: Bool {
-		guard  !token.isEmpty else { return false }
-		return true
-	}
+    var isAuthorized: Bool {
+        guard  !token.isEmpty else { return false }
+        return true
+    }
 }
 
 class AuthProvider: AuthProviderProtocol {
-	
-	var token: String = ""
-	var login: String = ""
-	
-	func loadToken() -> Bool {
-		token = UserDefaults.standard.string(forKey: "token") ?? ""
-		login = UserDefaults.standard.string(forKey: "login") ?? ""
-		
-		return !token.isEmpty && !login.isEmpty
-	}
-	
-	func authorize(with login: String, password: String, completion: @escaping (Bool) -> Void) {
-		Log.writeLog(logLevel: .info, message: "Fake token!")
-		self.login = login
-		self.token = UUID().uuidString
-		
-		UserDefaults.standard.set(login, forKey: "login")
-		UserDefaults.standard.set(token, forKey: "token")
-		completion(true)
-	}
-	
-	func logout(error: Swift.Error?) {
-		clear()
-		Log.writeLog(logLevel: .debug, message: "show auth view controller")
-		Router.presentAuth()
-	}
-	
-	func clear() {
-		token = ""
-		login = ""
-		UserDefaults.standard.set(nil, forKey: "login")
-		UserDefaults.standard.set(nil, forKey: "token")
-	}
+    private var configProvider: ConfigProviderProtocol!
+    var token: String = ""
+    var login: String = ""
+    
+    func loadToken() -> Bool {
+        
+        if configProvider == nil {
+            configProvider = DI.providers.resolve(ConfigProviderProtocol.self)
+        }
+        guard !configProvider.token.isEmpty else {
+            return false
+        }
+        token = configProvider.token
+        login = configProvider.login
+        return !configProvider.login.isEmpty && !configProvider.token.isEmpty
+    }
+    
+    func authorize(with login: String, password: String, completion: @escaping (Bool) -> Void) {
+        Log.writeLog(logLevel: .info, message: "Fake token!")
+        self.login = login
+        self.token = UUID().uuidString
+        configProvider.login = login
+        configProvider.token = token
+        configProvider.save { (success) in
+            if !success {
+                Log.writeLog(logLevel: .error, message: "Can't save login and token!")
+            }
+        }
+        completion(true)
+    }
+    
+    func logout(error: Swift.Error?) {
+        clear()
+        Log.writeLog(logLevel: .debug, message: "show auth view controller")
+        Router.presentAuth()
+    }
+    
+    func clear() {
+        configProvider.token = ""
+        configProvider.login = ""
+        configProvider.save(completion: nil)
+    }
 }
 
 extension AuthProvider {
-
-	enum Error: Swift.Error {
-		case cancelled
-		case needAuthorize
-	}
+    
+    enum Error: Swift.Error {
+        case cancelled
+        case needAuthorize
+    }
 }
