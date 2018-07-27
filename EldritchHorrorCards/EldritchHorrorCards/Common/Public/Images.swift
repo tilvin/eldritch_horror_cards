1d332f75c815c5bf7d6d90c0b2f011e7a6735768
Torlopov Andrey <Torlopov.andrey@gmail.com>
2018-07-27 11:19:11 +0300

    add images extension

---
+++ EldritchHorrorCards/EldritchHorrorCards/Common/Public/Images.swift
@@ -0,0 +1,20 @@
+//
+//  Images.swift
+//  EldritchHorrorCards
+//
+//  Created by Andrey Torlopov on 7/27/18.
+//  Copyright © 2018 Andrey Torlopov. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+	
+	static var artifact: UIImage {
+		return UIImage(named: "artifact")!
+	}
+	
+	static var asset: UIImage {
+		return UIImage(named: "asset")!
+	}
+}
