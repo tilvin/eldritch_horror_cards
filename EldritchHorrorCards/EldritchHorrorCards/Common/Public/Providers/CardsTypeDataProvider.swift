3082a93d9959822d1d394646a3521b9037b7eb2c
cinichka85 <cinichka85@gmail.com>
2018-07-28 12:00:33 +0300

    + fix cards type data (import from json)

--- EldritchHorrorCards/EldritchHorrorCards/Common/Public/Providers/CardsTypeDataProvider.swift
+++ EldritchHorrorCards/EldritchHorrorCards/Common/Public/Providers/CardsTypeDataProvider.swift
@@ -19,9 +19,30 @@ protocol CardsTypeDataProtocol {
 
 class CardsTypeDataProvider: CardsTypeDataProtocol {
     var cardShirt: [CardsType] = []
-    
+    private var cardsDataProvider = DI.providers.resolve(CardsDataProviderProtocol.self)!
+    private var portals: [StoryCard] = []
+    private var cities: [CityCard] = []
+    private var expeditions: [StoryCard] = []
+    private var contacts: [PlaceCard] = []
+    private var evidences: [PlaceCard] = []
+    private var specialContacts: [StoryCard] = []
     func load() {
+        cardsDataProvider.load { (success) in
+            if success {
+                Log.writeLog(logLevel: .debug, message: "Monster is load!")
+            }
+            else {
+                Log.writeLog(logLevel: .error, message: "Something gone wrong!")
+            }
+        }
+        portals = cardsDataProvider.portals
+        cities = cardsDataProvider.cities
+        expeditions = cardsDataProvider.expeditions
+        contacts = cardsDataProvider.contacts
+        evidences = cardsDataProvider.evidences
+        specialContacts = cardsDataProvider.specialContacts
+        print(portals)
         cardShirt = [CardsType(title:"Общие контакты", image: UIImage.contact),CardsType(title:"Контакты в Америке", image: UIImage.contactAmerica),CardsType(title:"Секретные карты Култху", image: UIImage.secretCthulhu),CardsType(title:"Поисковые карты Култху", image: UIImage.searchContactCthulhu),CardsType(title:"Специальные контакты Култху", image: UIImage.specialContactCthulhu)]
-        print(cardShirt)
-    }
+        
+}
 }
