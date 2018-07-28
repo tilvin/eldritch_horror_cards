00d0deab59f6789ffff9434f27ed3811c48d004c
Torlopov Andrey <Torlopov.andrey@gmail.com>
2018-07-28 16:19:03 +0300

    style fix

--- EldritchHorrorCards/EldritchHorrorCards/Common/Public/DI.swift
+++ EldritchHorrorCards/EldritchHorrorCards/Common/Public/DI.swift
@@ -1,25 +1,25 @@
 import Foundation
 
 class DI {
-    
-    static fileprivate let _di = DI()
-    
-    fileprivate let container = DIContainer()
-    
-    public static var providers: DIContainer {
-        return _di.container
-    }
-    
-    public static func registerProviders() {
-        _di.register()
-    }
-    
-    fileprivate func register() {
-        container.register(NavigatorProtocol.self, asSingleTone: true) { AppNavigator()  }
-        container.register(ConfigProviderProtocol.self, asSingleTone: true) { ConfigProvider() }
-        container.register(AuthProviderProtocol.self, asSingleTone: true) { AuthProvider() }
-        container.register(MonsterDataProviderProtocol.self, asSingleTone: true) { MonsterDataProvider() }
-        container.register(CardsDataProviderProtocol.self, asSingleTone: true) { CardsDataProvider() }
+	
+	static fileprivate let _di = DI()
+	
+	fileprivate let container = DIContainer()
+	
+	public static var providers: DIContainer {
+		return _di.container
+	}
+	
+	public static func registerProviders() {
+		_di.register()
+	}
+	
+	fileprivate func register() {
+		container.register(NavigatorProtocol.self, asSingleTone: true) { AppNavigator()  }
+		container.register(ConfigProviderProtocol.self, asSingleTone: true) { ConfigProvider() }
+		container.register(AuthProviderProtocol.self, asSingleTone: true) { AuthProvider() }
+		container.register(MonsterDataProviderProtocol.self, asSingleTone: true) { MonsterDataProvider() }
+		container.register(CardsDataProviderProtocol.self, asSingleTone: true) { CardsDataProvider() }
 		container.register(TabBarControllersProviderProtocol.self, asSingleTone: true) { TabBarControllersProvider() }
-    }
+	}
 }
