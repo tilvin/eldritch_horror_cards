9d121e5e74e73c91a84bd53f5659675c29eed528
cinichka85 <cinichka85@gmail.com>
2018-07-31 22:16:29 +0300

    unzip monsters

--- EldritchHorrorCards/EldritchHorrorCards/Common/Public/Providers/MonsterDataProvider.swift
+++ EldritchHorrorCards/EldritchHorrorCards/Common/Public/Providers/MonsterDataProvider.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Fakery
+import Zip
 
 protocol MonsterDataProviderProtocol {
     var monsters: [Monster] { get set }
@@ -16,14 +17,24 @@ protocol MonsterDataProviderProtocol {
 
 class MonsterDataProvider: MonsterDataProviderProtocol {
     var monsters: [Monster] = []
-    
+
     func load(completion: @escaping (Bool) -> Void) {
-        guard let path = Bundle.main.path(forResource: "monsters", ofType: "json"),
-            let data = try? Data(contentsOf: URL(fileURLWithPath: path), options: .alwaysMapped) else {
+        
+        do {
+            let filePath = Bundle.main.url(forResource: "monsters", withExtension: "zip")!
+            let file = try Zip.quickUnzipFile(filePath)
+            Log.writeLog(logLevel: .debug, message: "Zip... \(file)")
+        }
+        catch {
+            print("Something went wrong")
+        }
+        
+        let documentsUrl:URL =  (FileManager.default.urls(for: .documentDirectory, in: .allDomainsMask).first as URL?)!
+        let destinationFileUrl = documentsUrl.appendingPathComponent("monsters/monsters.json")
+         guard let data = try? Data(contentsOf: destinationFileUrl, options: .alwaysMapped) else {
                 print("can't parse json!")
                 return
         }
-        
         Log.writeLog(logLevel: .debug, message: "Json parsed... \(data)")
         
         if let json = try? JSONSerialization.jsonObject(with: data, options: [JSONSerialization.ReadingOptions.mutableContainers]) {
