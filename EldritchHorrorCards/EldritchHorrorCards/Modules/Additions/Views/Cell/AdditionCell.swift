9288e94d8086133962189dc3cc8ddab8a9774c8e
Torlopov Andrey <Torlopov.andrey@gmail.com>
2018-08-22 14:22:47 +0300

    addition cell fix constraint

--- EldritchHorrorCards/EldritchHorrorCards/Modules/Additions/Views/Cell/AdditionCell.swift
+++ EldritchHorrorCards/EldritchHorrorCards/Modules/Additions/Views/Cell/AdditionCell.swift
@@ -9,28 +9,11 @@
 import UIKit
 
 extension AdditionCell {
-	
+
 	struct Appearance {
-		let titleLabelColor = UIColor.black
-		let descriptionColor = UIColor.white
-		let descriptionFont = UIFont.bold28
-		let titleFont = UIFont.bold28
-		let spacingBetweenIconAndText: CGFloat = 8
-		let spacingBetweenTitleAndDescription: CGFloat = 4
-		let leftOrTopOffset = 16
-		var rightOrBottomOffset: Int { return -leftOrTopOffset }
-		
-		let roundedViewTopOffset = 8
-		var roundedViewBottomOffset: Int { return -roundedViewTopOffset }
-		
-		let roundedViewColor = UIColor.white
-		let backgroundColor = UIColor.clear
-		
-		let shadowOffset = CGSize(width: 0, height: 0)
-		let shadowOpacity: Float = 0.15
-		let shadowRadius: CGFloat = 12
-		let cornerRadius: CGFloat = 8
-		let shadowColor = UIColor.black
+//		let backgroundColor = UIColor.clear
+//		let cornerRadius: CGFloat = 8
+//		let shadowColor = UIColor.black
 	}
 }
 
@@ -40,77 +23,74 @@ protocol AdditionCellDelegate: class {
 }
 
 class AdditionCell: BaseTableViewCell {
-	
+
 	let appearance = Appearance()
 	var model: Addition!
 	var delegate: AdditionCellDelegate?
-	
+
 	lazy var titleLabel: UILabel = {
 		let view = UILabel()
 		view.font = UIFont.bold16
 		view.textColor = UIColor.mako
 		return view
 	}()
-	
+
 	lazy var selectSwitch: UISwitch = {
 		let view = UISwitch()
 		view.onTintColor = .darkGreenBlue
 		view.isOn = false
 		return view
 	}()
-	
+
 	lazy var infoButton: UIButton = {
 		let view = UIButton()
 		view.contentMode = .scaleAspectFit
 		view.setImage(UIImage.info, for: .normal)
 		return view
 	}()
-	
+
 	lazy var mapButton: UIButton = {
 		let view = UIButton()
 		view.contentMode = .scaleAspectFit
 		view.setImage(UIImage.mapOn, for: .normal)
 		return view
 	}()
-	
+
 	lazy var separateLine: UIView = {
 		let view = UIView()
 		view.backgroundColor = .alto
 		return view
 	}()
-	
+
 	lazy var stackView: UIStackView = {
 		var verticalStack = UIStackView()
 		verticalStack.axis = .vertical
-		verticalStack.distribution = .fill
-		verticalStack.spacing = 10
-		
+		verticalStack.distribution = .fillEqually
+
 		var titleStack = UIStackView()
 		titleStack.axis = .horizontal
 		titleStack.distribution = .fill
 		titleStack.alignment = .center
 		titleStack.spacing = 5
-		
+
 		titleStack.addArrangedSubview(titleLabel)
 		titleStack.addArrangedSubview(selectSwitch)
-		
+
 		var buttonStack = UIStackView()
 		buttonStack.axis = .horizontal
 		buttonStack.distribution = .fill
 		buttonStack.spacing = 20
-		
+
 		buttonStack.addArrangedSubview(infoButton)
 		buttonStack.addArrangedSubview(mapButton)
-		
-		let view = UIView()
-		buttonStack.addArrangedSubview(view)
-		
+		buttonStack.addArrangedSubview(UIView())
+
 		verticalStack.addArrangedSubview(titleStack)
 		verticalStack.addArrangedSubview(buttonStack)
-		
+
 		return verticalStack
 	}()
-	
+
 	override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
 		super.init(style: style, reuseIdentifier: reuseIdentifier)
 		addSubviews()
@@ -120,14 +100,14 @@ class AdditionCell: BaseTableViewCell {
 		infoButton.addTarget(self, action: #selector(infoPressed), for: .touchUpInside)
 		layoutIfNeeded()
 	}
-	
+
 	required init?(coder aDecoder: NSCoder) {
 		super.init(coder: aDecoder)
 		addSubviews()
 		makeConstraints()
 		layoutIfNeeded()
 	}
-	
+
 	public func update(with viewModel: Addition) {
 		model = viewModel
 		titleLabel.text = model.name
@@ -135,19 +115,19 @@ class AdditionCell: BaseTableViewCell {
 		mapButton.isHidden = !model.isMap
 		selectSwitch.isOn = model.isSelected
 	}
-	
+
 	private func addSubviews() {
 		addSubview(stackView)
 		addSubview(separateLine)
 	}
-	
+
 	private func makeConstraints() {
 		stackView.snp.makeConstraints { (make) in
 			make.left.right.equalToSuperview().inset(20)
 			make.top.equalToSuperview().inset(20)
 			make.bottom.equalToSuperview().inset(20)
 		}
-		
+
 		separateLine.snp.makeConstraints { (make) in
 			make.height.equalTo(1)
 			make.bottom.equalToSuperview()
@@ -155,19 +135,19 @@ class AdditionCell: BaseTableViewCell {
 			make.right.equalToSuperview()
 		}
 	}
-	
+
 	@objc private func switchPressed() {
 		print("switch! \(selectSwitch.isOn)")
 		model.isSelected = selectSwitch.isOn
 		delegate?.update(with: model)
 	}
-	
+
 	@objc private func mapPressed() {
 		print("map pressed")
 		model.isSelectedMap = mapButton.alpha == 1
 		delegate?.update(with: model)
 	}
-	
+
 	@objc private func infoPressed() {
 		print("indo pressed")
 		delegate?.infoPressed(with: model)
