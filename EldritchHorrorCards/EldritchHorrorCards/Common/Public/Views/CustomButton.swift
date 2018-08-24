441b8390519cf205027f9c766aff3b39b03d79b5
Torlopov Andrey <Torlopov.andrey@gmail.com>
2018-08-24 22:51:53 +0300

    try add gradient to custom button

--- EldritchHorrorCards/EldritchHorrorCards/Common/Public/Views/CustomButton.swift
+++ EldritchHorrorCards/EldritchHorrorCards/Common/Public/Views/CustomButton.swift
@@ -82,7 +82,7 @@ class CustomButton: UIButton {
 		updateAppearance()
 		updateStyles()
 	}
-	
+
 	//Private
 	
 	private func updateAppearance() {
@@ -94,5 +94,23 @@ class CustomButton: UIButton {
 		layer.cornerRadius = Constants.cornerRadius
 		layer.shadowOpacity = 0
 		layer.masksToBounds = false
+
+		//FIXME: не работает градиент!
+		if type == .darkGreenBlue {
+			let gradientLayer = CAGradientLayer()
+			gradientLayer.frame = self.frame
+			gradientLayer.colors = [UIColor.black.cgColor, UIColor.white.cgColor]
+			gradientLayer.locations = [0.1, 1.0]
+			gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
+			gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
+
+			self.layer.sublayers?.forEach({ (layer) in
+				if layer is CAGradientLayer {
+					layer.removeFromSuperlayer()
+				}
+			})
+
+			self.layer.insertSublayer(gradientLayer, at: 0)
+		}
 	}
 }
