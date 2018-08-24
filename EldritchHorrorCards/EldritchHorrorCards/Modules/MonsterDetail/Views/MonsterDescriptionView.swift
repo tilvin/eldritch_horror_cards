7474cb3489b0342b360685a932d83207bb8d9245
cinichka85 <cinichka85@gmail.com>
2018-08-24 10:32:15 +0300

    add struct Appearance in Monster Description

--- EldritchHorrorCards/EldritchHorrorCards/Modules/MonsterDetail/Views/MonsterDescriptionView.swift
+++ EldritchHorrorCards/EldritchHorrorCards/Modules/MonsterDetail/Views/MonsterDescriptionView.swift
@@ -9,6 +9,19 @@
 import UIKit
 import SnapKit
 
+struct Appearance {
+	let tableViewContentInsets = UIEdgeInsetsMake(0, 0, 0, 0)
+	let backgroundColorTextView = UIColor.darkGreenBlue
+	let textColor = UIColor.white
+	let cornerRadiusTextView: CGFloat = 15
+	let fontTextView = UIFont.regular14
+	let backgroundColor = UIColor.wildSand
+	let textViewlLeftOffset: CGFloat = 10
+	let textViewTopOffset: CGFloat = 60
+	let closeButtonWidth: CGFloat = 25
+	let closeButtonTop: CGFloat = 30
+	let closeButtonRight: CGFloat = 10
+}
 protocol MonsterDescriptionViewDelegate: class {
 	func closeButtonPressed()
 }
@@ -16,15 +29,16 @@ protocol MonsterDescriptionViewDelegate: class {
 class MonsterDescriptionView: UIView {
 	private var viewModel: Monster!
 	weak var delegate: MonsterDescriptionViewDelegate?
+	let appearance = Appearance()
 	
 	//MARK: -
 	
 	private lazy var descriptionTextView: UITextView = {
 		let tv = UITextView()
-		tv.font = UIFont.regular14
-		tv.backgroundColor = UIColor.darkGreenBlue
-		tv.textColor = UIColor.white
-		tv.layer.cornerRadius = 15
+		tv.font = appearance.fontTextView
+		tv.backgroundColor = appearance.backgroundColorTextView
+		tv.textColor = appearance.textColor
+		tv.layer.cornerRadius = appearance.cornerRadiusTextView
 		tv.clipsToBounds = true
 		tv.text = viewModel.other
 		return tv
@@ -39,7 +53,7 @@ class MonsterDescriptionView: UIView {
 	
 	init(frame: CGRect = CGRect.zero, viewModel: Monster) {
 		super.init(frame: frame)
-		backgroundColor = UIColor.wildSand
+		backgroundColor = appearance.backgroundColor
 		self.viewModel = viewModel
 		addSubviews()
 		makeConstraints()
@@ -60,14 +74,14 @@ class MonsterDescriptionView: UIView {
 	
 	private func makeConstraints() {
 		descriptionTextView.snp.makeConstraints { (make) in
-			make.left.right.bottom.equalToSuperview().inset(10)
-			make.top.equalToSuperview().inset(60)
+			make.left.right.bottom.equalToSuperview().inset(appearance.textViewlLeftOffset)
+			make.top.equalToSuperview().inset(appearance.textViewTopOffset)
 		}
 		
 		closeButtonView.snp.makeConstraints { (make) in
-			make.top.equalToSuperview().inset(30)
-			make.right.equalToSuperview().inset(10)
-			make.width.height.equalTo(25)
+			make.top.equalToSuperview().inset(appearance.closeButtonTop)
+			make.right.equalToSuperview().inset(appearance.closeButtonRight)
+			make.width.height.equalTo(appearance.closeButtonWidth)
 		}
 	}
 	
